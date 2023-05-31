dacb004f5bca5ef52498d763fba45322b255fe55
TendaniMamadi <mamadi.tendani@gmail.com>
2023-05-31 16:29:38 +0200

    final

--- bus_travel_DOM.js
+++ bus_travel_DOM.js
@@ -5,8 +5,18 @@ radioElem = document.querySelector(".times");
 calculateBtn = document.querySelector(".btn");
 returnElem = document.querySelector(".checked")
 
+singleTrip = document.querySelector(".single")
+priceTripElem = document.querySelector(".price_per_trip_single")
+number_of_return_trip = document.querySelector(".return_trips")
+returnPriceTripElem = document.querySelector(".price_per_trip_return")
+
 
 //create instance
 const busride = Busride()
 
 //DOM Events
+
+calculateBtn.addEventlistener("click",function(){
+    
+alert("clicked")
+})
